83125d623905f03b932cccc596cf4fde44b7dc15
boora-aman <aman.chaudharyboora@gmail.com>
2025-03-03 17:06:51 +0530

    added node version version and legacy peer

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+    webpack: (config) => {
+      // Increase memory limit for webpack
+      config.performance = {
+        ...config.performance,
+        maxEntrypointSize: 1024000,
+        maxAssetSize: 1024000
+      };
+      return config;
+    }
   }
-}
-
-export default nextConfig
+  
+  export default nextConfig
